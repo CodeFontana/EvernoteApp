333fbdd8bacf8761336832f1e3b8d172a5d81404
Brian Fontana <brian@codefoxtrot.com>
2022-03-18 19:36:25 -0400

    Set max char limit on Username

--- DataAccessLibrary/Entities/User.cs
+++ DataAccessLibrary/Entities/User.cs
@@ -12,6 +12,7 @@ public class User
     [MaxLength(50, ErrorMessage = "Last name must be less than 50 characters")]
     public string Lastname { get; set; }
 
+    [MaxLength(50, ErrorMessage = "User name must be less than 50 characters")]
     public string Username { get; set; }
 
     public string Password { get; set; }
