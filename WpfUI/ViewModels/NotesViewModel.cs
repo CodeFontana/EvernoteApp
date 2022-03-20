783e3e0e91d5157189678696909b681775bfe96e
Brian Fontana <brian@codefoxtrot.com>
2022-03-20 13:52:55 -0400

    Read notebooks and notes from database

--- WpfUI/ViewModels/NotesViewModel.cs
+++ WpfUI/ViewModels/NotesViewModel.cs
@@ -3,6 +3,7 @@ using DataAccessLibrary.Notes;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using WpfUI.Commands;
@@ -25,6 +26,9 @@ public class NotesViewModel : ViewModelBase
         NewNotebookCommand = new NewNotebookCommand(this);
         NewNoteCommand = new NewNoteCommand(this);
         ExitApplicationCommand = new ExitApplicationCommand();
+        Notebooks = new();
+        Notes = new();
+        GetNotebooks();
     }
 
     public ObservableCollection<Notebook> Notebooks { get; set; }
@@ -32,7 +36,12 @@ public class NotesViewModel : ViewModelBase
     public Notebook SelectedNotebook
     {
         get { return _selectedNotebook; }
-        set { _selectedNotebook = value; }
+        set 
+        { 
+            _selectedNotebook = value;
+            OnPropertyChanged("SelectedNotebook");
+            GetNotes();
+        }
     }
 
     public ObservableCollection<Note> Notes { get; set; }
@@ -54,6 +63,7 @@ public class NotesViewModel : ViewModelBase
         NotesDbContext db = scope.ServiceProvider.GetRequiredService<NotesDbContext>();
         await db.Notebooks.AddAsync(notebook);
         await db.SaveChangesAsync();
+        GetNotebooks();
     }
 
     public async Task CreateNote(int notebookId)
@@ -70,5 +80,26 @@ public class NotesViewModel : ViewModelBase
         NotesDbContext db = scope.ServiceProvider.GetRequiredService<NotesDbContext>();
         await db.Notes.AddAsync(newNote);
         await db.SaveChangesAsync();
+        GetNotes();
+    }
+
+    private void GetNotebooks()
+    {
+        using IServiceScope scope = _scopeFactory.CreateScope();
+        NotesDbContext db = scope.ServiceProvider.GetRequiredService<NotesDbContext>();
+        Notebooks.Clear();
+        db.Notebooks.ToList().ForEach(x => Notebooks.Add(x));
+    }
+
+    private void GetNotes()
+    {
+        if (SelectedNotebook != null)
+        {
+            using IServiceScope scope = _scopeFactory.CreateScope();
+            NotesDbContext db = scope.ServiceProvider.GetRequiredService<NotesDbContext>();
+            IQueryable<Note> notes = db.Notes.Where(x => x.NotebookId == SelectedNotebook.Id);
+            Notes.Clear();
+            notes.ToList().ForEach(x => Notes.Add(x));
+        }
     }
 }
