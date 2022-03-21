10b686673159a2ce1e2fa1f538adf9af37082a61
Brian Fontana <brian@codefoxtrot.com>
2022-03-21 19:07:31 -0400

    Add custom controls for Notebook and Note display

---
+++ WpfUI/Controls/NotebookControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary.Entities;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfUI.Controls;
+
+public partial class NotebookControl : UserControl
+{
+    public Notebook Notebook
+    {
+        get { return (Notebook)GetValue(NotebookProperty); }
+        set { SetValue(NotebookProperty, value); }
+    }
+
+    public static readonly DependencyProperty NotebookProperty =
+        DependencyProperty.Register("Notebook", typeof(Notebook), typeof(NotebookControl), new PropertyMetadata(null, SetValues));
+
+    private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        NotebookControl notebookControl = (NotebookControl)d;
+
+        if (notebookControl != null)
+        {
+            notebookControl.DataContext = notebookControl.Notebook;
+        }
+    }
+
+    public NotebookControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WpfUI/Controls/NotebookDisplayControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLibrary.Entities;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfUI.Controls;
+
+public partial class NoteControl : UserControl
+{
+    public Note Note
+    {
+        get { return (Note)GetValue(NoteProperty); }
+        set { SetValue(NoteProperty, value); }
+    }
+
+    public static readonly DependencyProperty NoteProperty =
+        DependencyProperty.Register("Note", typeof(Note), typeof(NoteControl), new PropertyMetadata(null, SetValues));
+
+    private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        NoteControl noteControl = (NoteControl)d;
+
+        if (noteControl != null)
+        {
+            noteControl.DataContext = noteControl.Note;
+        }
+    }
+
+    public NoteControl()
+    {
+        InitializeComponent();
+    }
+}
