329b749e19b5817827818d7e6f186d209aa513a3
Brian Fontana <brian@codefoxtrot.com>
2022-03-21 00:12:28 -0400

    Add NoteTextChangedCommand

---
+++ WpfUI/Commands/NoteTextChangedCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using WpfUI.ViewModels;
+
+namespace WpfUI.Commands;
+public class NoteTextChangedCommand : CommandBase
+{
+    private readonly NotesViewModel _notesViewModel;
+
+    public NoteTextChangedCommand(NotesViewModel notesViewModel)
+    {
+        _notesViewModel = notesViewModel;
+    }
+
+    public override void Execute(object parameter)
+    {
+        if (parameter is RichTextBox rtb)
+        {
+            _notesViewModel.CurrentNoteCharCount = (new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd)).Text.Length - 2;
+        }
+    }
+}
