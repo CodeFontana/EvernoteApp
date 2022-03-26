77871c2458d7e977d84539cc57e8c11ef8de1477
Brian Fontana <brian@codefoxtrot.com>
2022-03-26 16:17:55 -0400

    No need to set DataContext directly

--- WpfUI/Controls/NotebookControl.xaml.cs
+++ WpfUI/Controls/NotebookControl.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using DataAccessLibrary.Entities;
-using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,6 +6,11 @@ namespace WpfUI.Controls;
 
 public partial class NotebookControl : UserControl
 {
+    public NotebookControl()
+    {
+        InitializeComponent();
+    }
+
     public Notebook Notebook
     {
         get { return (Notebook)GetValue(NotebookProperty); }
@@ -14,20 +18,5 @@ public partial class NotebookControl : UserControl
     }
 
     public static readonly DependencyProperty NotebookProperty =
-        DependencyProperty.Register("Notebook", typeof(Notebook), typeof(NotebookControl), new PropertyMetadata(null, SetValues));
-
-    private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        NotebookControl notebookControl = (NotebookControl)d;
-
-        if (notebookControl != null)
-        {
-            notebookControl.DataContext = notebookControl.Notebook;
-        }
-    }
-
-    public NotebookControl()
-    {
-        InitializeComponent();
-    }
+        DependencyProperty.Register("Notebook", typeof(Notebook), typeof(NotebookControl), new PropertyMetadata(null));
 }
