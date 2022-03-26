e53f48c880a15da3d7789e08f341683ac680b2d4
Brian Fontana <brian@codefoxtrot.com>
2022-03-26 16:21:00 -0400

    Reduce Navigator to CurrentViewModel state
    Put MainViewModel in charge of initial navigation
    
    Leave it up to the NavigationCommand to perform nav

--- WpfUI/State/Navigator.cs
+++ WpfUI/State/Navigator.cs
@@ -1,21 +1,8 @@
-﻿using System.Windows.Input;
-using WpfUI.Commands;
-using WpfUI.ViewModels;
-using WpfUI.ViewModels.Factory;
+﻿using WpfUI.ViewModels;
 
 namespace WpfUI.State;
 
 public class Navigator
 {
-    private readonly ViewModelFactory _viewModelFactory;
-
-    public Navigator(ViewModelFactory viewModelFactory)
-    {
-        _viewModelFactory = viewModelFactory;
-        NavigateCommand = new NavigateCommand(this, _viewModelFactory);
-    }
-
     public ViewModelBase CurrentViewModel { get; set; }
-
-    public ICommand NavigateCommand { get; set; }
 }
--- WpfUI/ViewModels/MainViewModel.cs
+++ WpfUI/ViewModels/MainViewModel.cs
@@ -1,5 +1,5 @@
-﻿using DataAccessLibrary.Notes;
-using System;
+﻿using System.Windows.Input;
+using WpfUI.Commands;
 using WpfUI.State;
 using WpfUI.ViewModels.Factory;
 using static WpfUI.ViewModels.Factory.ViewModelFactory;
@@ -8,19 +8,14 @@ namespace WpfUI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private readonly ViewModelFactory _viewModelFactory;
-
-    public MainViewModel()
-    {
-        
-    }
-
     public MainViewModel(Navigator navigator, ViewModelFactory viewModelFactory)
     {
         Navigator = navigator;
-        _viewModelFactory = viewModelFactory;
-        navigator.CurrentViewModel = _viewModelFactory.CreateViewModel(ViewType.Notes);
+        NavigateCommand = new NavigateCommand(navigator, viewModelFactory);
+        NavigateCommand.Execute(ViewType.Notes);
     }
 
     public Navigator Navigator { get; }
+
+    public ICommand NavigateCommand { get; set; }
 }
